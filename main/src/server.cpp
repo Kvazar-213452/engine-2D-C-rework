1c941e4231b24215237c6bd6128d03a504916382
Conar365 <kvazar382@gmail.com>
2024-10-04 08:55:55 +0300

    lib

--- main/src/server.cpp
+++ main/src/server.cpp
@@ -17,12 +17,38 @@ std::string read_file(const std::string& filename) {
 
     std::stringstream buffer;
     buffer << file.rdbuf();
-    return buffer.str();
+    std::string content = buffer.str();
+
+    // Шаблон для пошуку тегів <% ('файл') %>
+    std::regex re(R"(<%\s*\(\s*'([^']*)'\s*\)\s*%>)");
+    std::smatch match;
+
+    // Шукати всі збіги
+    while (std::regex_search(content, match, re)) {
+        // Отримати шлях до файлу із шаблону
+        std::string include_filename = match[1].str();
+
+        // Зчитати вміст файлу
+        std::string include_content = read_file(include_filename);
+
+        // Замінити шаблон на вміст файлу
+        content.replace(match.position(0), match.length(0), include_content);
+
+        // Оновити пошук після заміни
+    }
+
+    return content;
 }
 
 void start_server() {
     httplib::Server svr;
 
+    // Встановлення статичної папки для css, js, зображень і т.д.
+    svr.set_mount_point("/css_st", "front/static/css");
+    svr.set_mount_point("/lib_st", "front/static/lib");
+    svr.set_mount_point("/js_st", "front/static/js");  // Додаємо мапінг для статичних файлів
+
+    // Читання index.html
     std::string html_content = read_file("front/index.html");
 
     svr.Get("/", [&html_content](const httplib::Request& req, httplib::Response& res) {
