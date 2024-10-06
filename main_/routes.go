2851d2b100d5c8643d5df885c29dba99603d5893
Conar365 <kvazar382@gmail.com>
2024-10-06 15:25:02 +0300

    go pug

--- main_/routes.go
+++ main_/routes.go
@@ -5,14 +5,27 @@ import (
 	"net/http"
 )
 
+type PageVariables struct {
+	Title string
+	Body  string
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("front/page/index.pug")
+	pageVariables := PageVariables{
+		Title: "Моя перша веб-сторінка",
+		Body:  "Вітаю на моїй веб-сторінці!",
+	}
+
+	// Завантаження шаблонів
+	tmpl, err := template.ParseFiles("front/page/header.pug", "front/page/index.pug")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if err := tmpl.Execute(w, map[string]string{"Title": "title", "Body": "body"}); err != nil {
+	// Виконання шаблону
+	err = tmpl.ExecuteTemplate(w, "index.pug", pageVariables)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
